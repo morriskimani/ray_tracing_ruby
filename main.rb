aebc0ab963ddd270ba5b8f9d9ffa4e9fa9601772
Morris Kimani <20963766+morriskimani@users.noreply.github.com>
2023-09-19 16:41:50 +0300

    rewrite first ppm image generation using Vec3 and Color classes

--- main.rb
+++ main.rb
@@ -1,20 +1,20 @@
+require_relative './vec3'
+require_relative './color'
+
+# Image
 image_height = 256
 image_width = 256
 
+# Render
 File.open('image.ppm', 'w') do |file|
   file << "P3\n" << image_width << ' ' << image_height << "\n255\n"
 
   (0...image_height).each do |row|
+    print("\rScanlines remaining:  #{image_height - 1} ")
+
     (0...image_width).each do |col|
-      r = col.to_f / (image_width - 1)
-      g = row.to_f / (image_height - 1)
-      b = 0
-
-      color_to_int = ->(color) { (255.999 * color).to_i }
-
-      ir, ig, ib = [r, g, b].map(&color_to_int)
-
-      file << ir << ' ' << ig << ' ' << ib << "\n"
+      pixel_color = Color.new(col.to_f / (image_width - 1), row.to_f / (image_height - 1), 0)
+      ColorUtils.write_color(file, pixel_color)
     end
   end
 end
