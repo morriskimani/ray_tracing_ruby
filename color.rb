2403a0053e58a10eabf0526e016f06a06ef0784d
Morris Kimani <20963766+morriskimani@users.noreply.github.com>
2023-09-22 11:14:51 +0300

    use gamma correction for accurate color intensity

--- color.rb
+++ color.rb
@@ -15,12 +15,23 @@ module ColorUtils
       g *= scale
       b *= scale
 
+      # Apply linear to gamma transform
+      r = linear_to_gamma(r)
+      g = linear_to_gamma(g)
+      b = linear_to_gamma(b)
+
       intensity = Interval.new(0.0, 0.999)
 
       stream \
         << (256 * intensity.clamp(r)).to_i << ' ' \
         << (256 * intensity.clamp(g)).to_i << ' ' \
         << (256 * intensity.clamp(b)).to_i << "\n"
+    end
+
+    # Transform from linear space to Gamma space
+
+    def linear_to_gamma(linear_component)
+      Math.sqrt(linear_component)
     end
   end
 end
