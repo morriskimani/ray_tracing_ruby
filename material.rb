643b5f9830f77888918b96a6093f38084ff2213f
Morris Kimani <20963766+morriskimani@users.noreply.github.com>
2023-09-25 15:44:00 +0300

    refactor materials code and add more docs

--- material.rb
+++ material.rb
@@ -11,20 +11,32 @@ class Material
     :is_scattered # true if the ray was scattered
   )
 
-  def initialize(albedo)
-    @albedo = albedo
+  def initialize(*)
     @scatter_record = ScatterRecord.new
   end
-  attr_reader :albedo, :scatter_record
 
+  # Scatter a ray (r_in) that has hit the material.
+  #
+  # @param r_in [Ray] the incident ray
+  # @param hit_record [HitRecord] information about the incidence.
+  # @return [ScatterRecord]
   def scatter(r_in, hit_record)
     raise NotImplementedError
   end
+
+  protected
+
+  attr_reader :scatter_record
 end
 
 # Models a Lambertian material (A diffuse surface)
 # Such a material scatters incident illumination equally in all directions.
 class Lambertian < Material
+  def initialize(albedo)
+    super
+    @albedo = albedo
+  end
+
   def scatter(_, hit_record)
     scatter_direction = hit_record.normal + random_unit_vector
 
@@ -32,7 +44,7 @@ class Lambertian < Material
     scatter_direction = hit_record.normal if scatter_direction.near_zero?
 
     scatter_record.scattered_ray = Ray.new(hit_record.p, scatter_direction)
-    scatter_record.attenuation = albedo
+    scatter_record.attenuation = @albedo
     scatter_record.is_scattered = true
 
     scatter_record
@@ -41,11 +53,16 @@ end
 
 # Models materials that have polished surfaces
 class Metal < Material
+  def initialize(albedo)
+    super
+    @albedo = albedo
+  end
+
   def scatter(r_in, hit_record)
     reflected = reflect(unit_vector(r_in.direction), hit_record.normal)
 
     scatter_record.scattered_ray = Ray.new(hit_record.p, reflected)
-    scatter_record.attenuation = albedo
+    scatter_record.attenuation = @albedo
     scatter_record.is_scattered = true
 
     scatter_record
